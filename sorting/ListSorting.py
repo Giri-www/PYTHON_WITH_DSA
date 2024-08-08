9dea80193f22df40088a0ace0cd0613c7ac381bc
GIRI RAHUL <girirahul944@gmail.com>
2024-08-08 14:29:12 +0530

    added sorting with key function

--- sorting/ListSorting.py
+++ sorting/ListSorting.py
@@ -8,7 +8,7 @@ print(numbers)  # [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
 
 
 #### Sorted With Key Function To Sort Complex DS Like Tuple,List And Dict ####
-students = [("Giri", "A", 15), ("Das", "B", 12), ("Paul", "B", 10)]
+students = [("Giri", "A", 15), ("Das", "C", 12), ("Paul", "B", 10)]
 
  
 sorted_by_age = sorted(students, key=lambda student: student[2])
